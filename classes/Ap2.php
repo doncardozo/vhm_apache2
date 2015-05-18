fe7a69c203dacdaca3021edfe4dae9afb712d900
Tony <tony@shopear.com>
2015-05-18 16:59:28 -0300

    Update Ap2.php

--- classes/Ap2.php
+++ classes/Ap2.php
@@ -45,42 +45,21 @@ class Ap2 implements InterfaceAction, InterfaceParams {
 
         $this->_default = <<<A
 <VirtualHost *:{$this->_port}>
-	ServerAdmin webmaster@localhost
-        ServerName {$this->_server_name}
-	DocumentRoot {$this->_document_root}
-        SetEnv "development"
+
+	   ServerAdmin webmaster@localhost
+       ServerName {$this->_server_name}
+       DocumentRoot {$this->_document_root}
+       SetEnv "development"
         
-	<Directory {$this->_document_root}>
-		Options Indexes FollowSymLinks MultiViews
+	<Directory {$this->_document_root}/>		
 		AllowOverride All
 		Order allow,deny
 		allow from all
-	</Directory>
-
-	ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
-	<Directory "/usr/lib/cgi-bin">
-		AllowOverride None
-		Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
-		Order allow,deny
-		Allow from all
+        Options Indexes FollowSymLinks
 	</Directory>
 
 	ErrorLog {$APACHE_LOG_DIR}/error.log
-
-	# Possible values include: debug, info, notice, warn, error, crit,
-	# alert, emerg.
-	LogLevel warn
-
 	CustomLog {$APACHE_LOG_DIR}/access.log combined
-
-    Alias /doc/ "/usr/share/doc/"
-    <Directory "/usr/share/doc/">
-        Options Indexes MultiViews FollowSymLinks
-        AllowOverride None
-        Order deny,allow
-        Deny from all
-        Allow from 127.0.0.0/255.0.0.0 ::1/128
-    </Directory>
 
 </VirtualHost>
 A
